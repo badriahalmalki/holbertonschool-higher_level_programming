6d9a9e3ec2436a63e6a6bab8cb8b9234f83b5a94
badriahalmalki <badryahbalmalki@gmail.com>
2026-01-17 16:21:45 +0300

    fixe line no

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-# Slice the string to get the first 3 letters, last 2 letters, and the middle part
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
