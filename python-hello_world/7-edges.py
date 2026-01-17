cdf98a71902e514c1b34c9455e4771ff0a4d71dc
badriahalmalki <badryahbalmalki@gmail.com>
2026-01-17 15:31:34 +0300

    move the files to the right repo

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+# Slice the string to get the first 3 letters, last 2 letters, and the middle part
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
---
+++ python-hello_world/9-easter_egg.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# Easter egg: The Zen of Python
+import this
