b4e790c05f6bd309edf7067698507cd4b092c051
badriahalmalki <badryahbalmalki@gmail.com>
2026-01-24 19:34:01 +0300

    test

---
+++ python-data_structures/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# practicing input() function
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(f"Hello, {name}!")
+print(f"You are {age} years old.")
+
