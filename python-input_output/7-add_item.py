93f2b774557e985379c19f288cb0e0c6477f5666
badriahalmalki <badryahbalmalki@gmail.com>
2026-02-01 09:09:18 +0300

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ to a JSON file named add_item.json.
 """
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
