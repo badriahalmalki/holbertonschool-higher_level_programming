81f219e931d6a4addf8f85dc80db64561b9dc615
badriahalmalki <badryahbalmalki@gmail.com>
2026-02-28 21:28:15 +0300

    add files for test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
+"""List all states from the hbtn_0e_0_usa database.
+
+The script connects to a local MySQL server and prints each row from the
+states table ordered by id in ascending order.
+"""
+
 import MySQLdb
+import sys
+
+
+def fetch_states(user, password, db_name):
+    """Connect to MySQL and return a tuple list of rows from states ordered by id."""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=password, db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    cur.close()
+    db.close()
+    return rows
+
+
+def main():
+    """Read command-line arguments and print each state row as a tuple."""
+    if len(sys.argv) != 4:
+        return
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    for row in fetch_states(user, password, db_name):
+        print(row)
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    main()
