a2965ed883716734620ec06e0d629eea606a7bf6
badriahalmalki <badryahbalmalki@gmail.com>
2026-02-28 21:41:00 +0300

    add files for test

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+"""Filter states by name from the hbtn_0e_0_usa database.
+
+The script connects to a local MySQL server and prints each row from the
+states table where the name matches the provided argument. Rows are ordered
+by id in ascending order.
+"""
+
 import sys
+
 import MySQLdb
 
+
+def fetch_states_by_name(user, password, db_name, state_name):
+    """Return rows from states where name matches state_name ordered by id."""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=user,
+                         passwd=password,
+                         db=db_name)
+    cur = db.cursor()
+    query = ("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+             .format(state_name))
+    cur.execute(query)
+    rows = cur.fetchall()
+    cur.close()
+    db.close()
+    return rows
+
+
+def main():
+    """Read CLI args and print each matching state row as a tuple."""
+    if len(sys.argv) != 4 and len(sys.argv) != 5:
+        return
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    for row in fetch_states_by_name(user, password, db_name, state_name):
+        print(row)
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    main()
