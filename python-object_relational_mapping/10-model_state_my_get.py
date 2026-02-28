dccc834db85125f4b53f842afad4e748bc3e7749
badriahalmalki <badryahbalmalki@gmail.com>
2026-02-28 22:00:06 +0300

    add files for test

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,53 @@
 #!/usr/bin/python3
-# Lists the State object with the name passed as argument
-# from the database hbtn_0e_6_usa.
-# Usage: ./10-model_state_my_get.py <mysql username> /
-#                                   <mysql password> /
-#                                   <database name>
-#                                   <state name searched>
+"""Print the id of a State object with a given name from hbtn_0e_6_usa.
+
+Connects to a local MySQL server and prints the id of the State object
+whose name matches the argument. The query is SQL injection safe.
+If no matching state exists, prints 'Not found'.
+"""
+
 import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+from model_state import Base, State
+
+
+def main():
+    """Retrieve and print the id of the State with the given name."""
+    if len(sys.argv) != 5:
+        return
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            user, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-    if found is False:
+    state = (
+        session.query(State)
+        .filter(State.name == state_name)
+        .order_by(State.id)
+        .first()
+    )
+
+    if state:
+        print(state.id)
+    else:
         print("Not found")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,49 @@
 #!/usr/bin/python3
-# Lists all State objects that contain the letter a
-# from the database hbtn_0e_6_usa.
-# Usage: ./9-model_state_filter_a.py <mysql username> /
-#                                    <mysql password> /
-#                                    <database name>
+"""List all State objects containing the letter 'a' from hbtn_0e_6_usa.
+
+Connects to a local MySQL server and prints all State objects whose
+name contains the letter 'a', ordered by states.id in ascending order.
+"""
+
 import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+from model_state import Base, State
+
+
+def main():
+    """Retrieve and print all State objects containing 'a'."""
+    if len(sys.argv) != 4:
+        return
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            user, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+    states = (
+        session.query(State)
+        .filter(State.name.like('%a%'))
+        .order_by(State.id)
+        .all()
+    )
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
