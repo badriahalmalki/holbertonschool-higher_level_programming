7da4d61dde7f66c2eb9728dc00160002c3b1366e
badriahalmalki <badryahbalmalki@gmail.com>
2026-02-28 22:13:25 +0300

    test

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Script that inserts a new row in the table first_table
+-- New row: id = 89, name = Best School
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
